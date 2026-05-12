930b577f2114b01a9c81c235cb82b07138c986e4
phamhoangviet261 <hoangviet.it.hcmus@gmail.com>
2026-05-12 17:36:38 +0700

    remove Cloudflare and Lovable dev dependencies and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart({
-      server: { entry: "server" },
+      server: {
+        entry: "server",
+      },
     }),
     nitro({
       preset: "vercel",
